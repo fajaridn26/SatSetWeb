f69118f7abbf72d9d643e9320043c82574f892a6
Fajar Indrawan <fajaridn@student.ittelkom-sby.ac.id>
2022-06-02 14:58:51 +0700

    first commit

---
+++ app/Views/home/index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>SatSet Website</title>
+  <meta content="SatSet Website adalah website yang menyediakan jasa layanan pembuatan website mulai dari daftar list sampai pembuatan website custom" name="description">
+  <meta content="Satset,jasa,pembuatan,website, murah" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?= base_url(); ?>/assets/img/Icon/icons8-lightning-bolt-color-glass-16.png" rel="icon">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url(); ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>/assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center justify-content-between">
+      <h1 class="logo"><a href="index.html"><img src="<?= base_url(); ?>/assets/img/Icon/icons8-lightning-bolt-color-glass-32.png" alt="" class="img-fluid"> &nbsp; Satset Website</a></h1>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#paket">Paket Website</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#ulasan">ulasan</a></li>
+          <li><a class="nav-link scrollto" href="https://api.whatsapp.com/send?phone=6281936666423&text=Halo,%20saya%20mau%20bertanya%20tentang%20jasa%20pembuatan%20website" target="_blank">Hubungi Kami</a></li>
+          <li><a class="nav-link scrollto" href="#login">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div id="heroCarousel" class="carousel slide-1" data-bs-ride="carousel">
+      <div class="carousel-item active" style="background-image: url(./assets/img/background/ilya-pavlov-OqtafYT5kTw-unsplash.jpg)">
+        <div class="carousel-container">
+          <div class="container">
+            <h2>SatSet Website</h2>
+            <p>Melayani Jasa Pembuatan Berbagai Macam Website</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero -->
+
+  <!-- ======= Daftar Paket Section ======= -->
+    <section id="paket" class="ulasan section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>Paket</h2>
+          <h3>Daftar Paket</h3>
+          <p>Berikut ini adalah beberapa paket yang kami sediakan.</p>
+        </div>
+
+        <div class="row justify-content-center mx-3">
+
+          <!-- Paket 1 -->
+          <div class="col-sm-3">
+            <div class="card text-center">
+              <h5 class="card-header bg-primary text-light fw-bold pt-4 pb-4">
+                Blog Pribadi
+              </h5>
+              <div class="card-body">
+                <h2 class="card-title fw-bold mt-3 mb-4">Rp42.075,00</h2>
+                <p class="card-text">Cocok untuk anda yang hobi menuangkan pemikiran baik dalam bentuk tulisan seperti berita, ataupun opini pribadi.</p>
+              </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">up to 4 Halaman Awal</li>
+                  <li class="list-group-item">1x Revisi Desain</li>
+                  <li class="list-group-item">SSD Storage 500 MB</li>
+                  <li class="list-group-item">Free Setup 6 Konten</li>
+                  <li class="list-group-item">Free G-Analytic & Webmaster</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Gratis SSL Selamanya</li>
+                  <li class="list-group-item">Mobile Friendly</li>
+                </ul>
+              <div class="card-body">
+                <a href="https://wa.me/message/NWZAHPLLWVHSL1" target="_blank" class="btn btn-primary rounded-pill my-2 px-4"> Order Sekarang </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Paket 2 -->
+          <div class="col-sm-3">
+            <div class="card text-center">
+              <h5 class="card-header bg-primary text-light fw-bold pt-4 pb-4">
+                Website Perusahaan
+              </h5>
+              <div class="card-body">
+                <h2 class="card-title fw-bold mt-3 mb-4">Rp99.552,00</h2>
+                <p class="card-text">Cocok untuk anda selaku pebisnis atau perusahaan yang menjual produk atau jasa secara B2B seperti kontraktor, konsultan, dll.</p>
+              </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">up to 4 Halaman Awal</li>
+                  <li class="list-group-item">1x Revisi Desain</li>
+                  <li class="list-group-item">Unlimited Email Bisnis</li>
+                  <li class="list-group-item">Unlimited SSD Storage</li>
+                  <li class="list-group-item">Gratis Maintenance</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Gratis SSL Selamanya</li>
+                  <li class="list-group-item">Mobile Friendly</li>
+                </ul>
+              <div class="card-body">
+                <a href="https://wa.me/message/NWZAHPLLWVHSL1" target="_blank" class="btn btn-primary rounded-pill my-2 px-4"> Order Sekarang </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Paket 3 -->
+          <div class="col-sm-3">
+            <div class="card text-center">
+              <h5 class="card-header bg-primary text-light fw-bold pt-4 pb-4">
+                Toko Online
+              </h5>
+              <div class="card-body">
+                <h2 class="card-title fw-bold mt-3 mb-4">Rp95.265,00</h2>
+                <p class="card-text">Cocok untuk anda selaku pebisnis atau UMKM yang menjual produk secara langsung seperti fashion, kuliner, dll.</p>
+              </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">up to 4 Halaman Awal</li>
+                  <li class="list-group-item">1x Revisi Desain</li>
+                  <li class="list-group-item">Unlimited SSD Storage</li>
+                  <li class="list-group-item">Pembayaran Otomatis</li>
+                  <li class="list-group-item">Integrasi Marketplace</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Gratis SSL Selamanya</li>
+                  <li class="list-group-item">Mobile Friendly</li>
+                </ul>
+              <div class="card-body">
+                <a href="https://wa.me/message/NWZAHPLLWVHSL1" target="_blank" class="btn btn-primary rounded-pill my-2 px-4"> Order Sekarang </a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Paket 4 -->
+          <div class="col-sm-3">
+            <div class="card text-center">
+              <h5 class="card-header bg-primary text-light fw-bold pt-4 pb-4">
+                Website Donasi
+              </h5>
+              <div class="card-body">
+                <h2 class="card-title fw-bold mt-3 mb-4">Rp149.681,00</h2>
+                <p class="card-text">Cocok untuk anda yang memiliki yayasan, pondok pesantren, organisasi non-profit, panitia dewan kemakmuran masjid, dll.</p>
+              </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">up to 6 Halaman Awal</li>
+                  <li class="list-group-item">2x Revisi Desain</li>
+                  <li class="list-group-item">SSD Storage 500 MB</li>
+                  <li class="list-group-item">Free Setup 6 Konten</li>
+                  <li class="list-group-item">Free G-Analytic & Webmaster</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Gratis SSL Selamanya</li>
+                  <li class="list-group-item">Mobile Friendly</li>
+                </ul>
+              <div class="card-body">
+                <a href="https://wa.me/message/NWZAHPLLWVHSL1" target="_blank" class="btn btn-primary rounded-pill my-2 px-4"> Order Sekarang </a>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section><!-- End Daftar Paket Section -->
+
+  <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container-fluid">
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <h3>Daftar Portofolio</h3>
+          <p>Ini adalah beberapa Portofolio Web yang sudah kami kerjakan</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter=".all">All</li>
+              <li data-filter=".tokoonline">Toko Online</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container justify-content-center">
+          <div class="col-lg-11">
+            <div class="row justify-content-center">
+              <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item tokoonline">
+                <div class="portfolio-wrap border">
+                  <img src="<?= base_url(); ?>/assets/img/portfolio/portfolio1.png" class="img-fluid" alt="">
+                  <div class="portfolio-info">
+                    <h4>Bakery APP</h4>
+                  </div>
+                </div>
+              </div><!-- End portfolio item -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Ulasan Section ======= -->
+    <section id="ulasan" class="ulasan section-bg">
+      <div class="container-fluid">
+        <div class="section-title">
+          <h2>Ulasan</h2>
+          <h3>Daftar Ulasan</h3>
+          <p>Berikut ini adalah beberapa ulasan dari pelanggan kami.</p>
+        </div>
+
+        <div class="justify-content-center">
+          <div class="testimonial-item text-center">
+            <img src="<?= base_url(); ?>/assets/ulasan/ulasan 1.png" class="testimonial-img" alt="">
+          </div>
+        </div><!-- End testimonial-item -->  
+      </div>
+    </section><!-- End Ulasan Section -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+
+              <div class="col-lg-5 footer-info">
+                <h4>About SatSet Website</h4>
+                <p>
+                  Satset Website adalah Website yang <br>
+                  menyediakan layanan jasa pembuatan <br> 
+                  macam website mulai dari paket yang <br>
+                  tersedia hingga custom website</p>
+                <div class="social-links mt-3">
+                  <a href="https://id-id.facebook.com/fajaridn" class="facebook" target="_blank"><i class="bi bi-facebook"></i></a>
+                  <a href="https://instagram.com/fajaridn/" class="instagram" target="_blank"><i class="bi bi-instagram"></i></i></a>
+                  <a href="https://wa.me/6281936666423" class="whatsapp" target="_blank"><i class="bi bi-whatsapp"></i></i></a>
+                </div>
+              </div>
+              <div class="col-lg-4 footer-links">
+                <h4>Useful Links</h4>
+                <p> </p>
+                <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#paket">Paket Website</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#portfolio">Portfolio</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#ulasan">Ulasan</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="https://wa.me/message/NWZAHPLLWVHSL1">Hubungi Kami</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#login">Login</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-3 footer-contact">
+                <h4>Contact Us</h4>
+                <p>
+                  Ketintang No.156<br>
+                  Surabaya, Jawa Timur<br>
+                  Indonesia <br>
+                  <strong>Phone : </strong><a href="telp:6281936666423">+62819-3666-6423</a><br>
+                  <strong>Email : </strong><a href="mailto:satsetwebsite@gmail.com">satsetwebsite@gmail.com</a><br>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>SatSet Website</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by SatSet Website
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?= base_url(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>  
+  <script src="<?= base_url(); ?>/assets/js/main.js"></script>
+</body>
+</html>
